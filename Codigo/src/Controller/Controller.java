e19a635ff8006a595d6c6bbbb87313ecc1565bce
Santiago Granobles <santiago.granobles@hotmail.com>
2021-04-14 22:07:13 -0500

    Controller done

--- Codigo/src/Controller/Controller.java
+++ Codigo/src/Controller/Controller.java
@@ -17,4 +17,46 @@ public class Controller {
 		
 	}
 
+	public void paintPsique() {
+		interaction.paintPsique();	
+	}
+	
+	public void dragPsique() {
+		
+		interaction.dragPsique();
+	}
+	
+	public void paintKey() {
+		interaction.paintKey();
+	}
+	
+	public void keyInteraction() {
+		interaction.keyInteraction();
+	}
+	
+	public void paintNAF() {
+		interaction.paintNAF();
+	}
+	
+	public void nafInteraction() {
+		interaction.nafInteraction();
+	}
+	
+	public void paintEros() {
+		interaction.paintEros();
+	}
+	
+	public void erosInteraction() {
+		interaction.erosInteraction();
+	}
+	
+	public void paintOilDrop() {
+		interaction.paintOilDrop();
+	}
+	
+	public void dragOilDrop() {
+		
+		interaction.dragOilDrop();
+	}
+	
 }
